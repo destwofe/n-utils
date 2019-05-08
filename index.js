62c141ff37d805ecefb748f8b8e182ecd7451c86
Napat Yimjan <destwofe@gmail.com>
2019-05-08 12:37:43 +0700

    add function array compare and object split

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const getSafe = fn => {
 };
 
 /**
+ * Compare two array
+ * @param {Array} arr1
+ * @param {Array} arr2
+ */
+const arrayCompare = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return false;
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+/**
  * get request remote ip address
  * @param {Request} req
  */
@@ -26,8 +40,39 @@ const objectify = err =>
     ? { name: err.name, message: err.message, stack: err.stack, ...err }
     : err;
 
+/**
+ * split array of object to array of array of object
+ * ex [{a: 1, b:1}, {a: 2, b: 2}, {c: 3}, {d: 1, e: 1}] ->
+ * [[{a: 1, b:1}, {a: 2, b: 2}], [{c: 2}], [{d: 1, e: 1}]]
+ * @param {Array} arr
+ */
+const objectSplit = (arr) => {
+  try {
+    if (!Array.isArray(arr)) return undefined;
+    if (arr.length === 0) return [];
+    const tables = [];
+    let tableIndex = -1;
+    let previousKeys = null;
+    arr.forEach(obj => {
+      const keys = Object.keys(obj);
+      if (!arrayCompare(previousKeys, keys)) {
+        tables.push([obj]);
+        tableIndex += 1;
+      } else {
+        tables[tableIndex].push(obj);
+      }
+      previousKeys = keys;
+    })
+    return tables;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   getSafe,
+  arrayCompare,
   getIPAddr,
   objectify,
+  objectSplit,
 };
